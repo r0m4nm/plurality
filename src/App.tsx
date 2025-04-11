01fc46c32740dcf0f891598baad1d7f7f6817e6e
r0m4nm <romsenmoor@gmail.com>
2025-04-11 16:45:05 +0200

    Comment out PluralityWallet component in App.tsx and add a placeholder heading "Hello World".

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { PluralityWallet } from "./PluralityWallet";
 const App = () => {
     return (
         <div>
-            <PluralityWallet
+            {/* <PluralityWallet
                 clientId=""
                 text="ahdkj"
                 onLogin={() => {
                     console.log("login");
                 }}
-            />
+            /> */}
+            <h1>Hello World</h1>
         </div>
     );
 };
